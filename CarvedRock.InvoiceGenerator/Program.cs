7420ff4f822c49834b71ea5c9a527129832e747f
Robin Bette <robinbette@hotmail.com>
2022-01-31 17:51:37 +0100

    Set up logging for a console application

--- CarvedRock.InvoiceGenerator/Program.cs
+++ CarvedRock.InvoiceGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Serilog;
 
 namespace CarvedRock.InvoiceGenerator
 {
@@ -6,7 +7,38 @@ namespace CarvedRock.InvoiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConfigureLogging();
+
+            try
+            {
+                Log.ForContext("Args", args)
+                   .Information("Starting program...");
+
+                Console.WriteLine("Hello World!"); // do some invoice generation
+
+                Log.Information("Finished execution!");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Some kind of exception occurred.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void ConfigureLogging()
+        {
+            var name = typeof(Program).Assembly.GetName().Name;
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Assembly", name)
+                .WriteTo.Console()
+                .WriteTo.Seq("http://host.docker.internal:5341")
+                .CreateLogger();
         }
     }
 }
