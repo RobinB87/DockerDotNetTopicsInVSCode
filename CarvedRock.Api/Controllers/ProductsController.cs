6edaca45f591601760c560777de74c4c03528c00
Robin Bette <robinbette@hotmail.com>
2022-01-31 15:06:39 +0100

    Update controller to use static class directly

--- CarvedRock.Api/Controllers/ProductsController.cs
+++ CarvedRock.Api/Controllers/ProductsController.cs
@@ -2,7 +2,7 @@
 using CarvedRock.Api.ApiModels;
 using CarvedRock.Api.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace CarvedRock.Api.Controllers
 {
@@ -10,19 +10,17 @@ namespace CarvedRock.Api.Controllers
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly ILogger<ProductsController> _logger;
         private readonly IProductLogic _productLogic;
 
-        public ProductsController(ILogger<ProductsController> logger, IProductLogic productLogic)
+        public ProductsController(IProductLogic productLogic)
         {
-            _logger = logger;
             _productLogic = productLogic;
         }
 
         [HttpGet]
         public IEnumerable<Product> GetProducts(string category = "all")
         {
-            _logger.LogInformation("Starting controller action GetProducts");
+            Log.Information("Starting controller action GetProducts");
             return _productLogic.GetProductsForCategory(category);
         }
     }
